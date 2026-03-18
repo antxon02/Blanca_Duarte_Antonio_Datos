f941abbec2a9056c956676bc2fb9a41de60b1e85
nitrozeus666 <rubendelensanchez233@gmail.com>
2026-03-18 08:40:26 +0100

    He añadido el botón del ejercicio1 al MainActivity.kt

--- app/src/main/java/com/antonio/blanca_duarte_antonio_datos/MainActivity.kt
+++ app/src/main/java/com/antonio/blanca_duarte_antonio_datos/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.antonio.blanca_duarte_antonio_datos
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 
@@ -8,5 +9,12 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn1 = findViewById<android.widget.Button>(R.id.btnEjercicio1)
+
+        btn1.setOnClickListener {
+            val intent = Intent(this, Ejercicio1Activity::class.java)
+            startActivity(intent)
+        }
     }
 }
